1d2740de5c9c1eb9df3dd482953e66f6b9cc953a
Dylan Bourque <dylan.bourque@crowdstrike.com>
2023-03-08 15:43:31 -0600

    updated `scripts/processmod.sh` to be clean under `shellcheck`

--- scripts/processmod.sh
+++ scripts/processmod.sh
@@ -2,12 +2,12 @@
 
 function cleanup {
     # restore Git branch and PWD if needed
-    if [ -n "${_PERSEUS_INITIAL_BRANCH}"  -a "${_PERSEUS_INITIAL_BRANCH}" != "$(git rev-parse --abbrev-ref HEAD)" ]; then
+    if [ -n "${_PERSEUS_INITIAL_BRANCH}" ] && [ "${_PERSEUS_INITIAL_BRANCH}" != "$(git rev-parse --abbrev-ref HEAD)" ]; then
         echo "Resetting Git branch to ${_PERSEUS_INITIAL_BRANCH}"
-        git checkout -q ${_PERSEUS_INITIAL_BRANCH}
+        git checkout -q "${_PERSEUS_INITIAL_BRANCH}"
     fi
     if [ -n "${_PERSEUS_START_DIR}" ]; then
-        cd ${_PERSEUS_START_DIR}
+        cd "${_PERSEUS_START_DIR}"
     fi
 }
 trap cleanup EXIT
@@ -24,7 +24,7 @@ do
         ;;
     -s|--server)
         PERSEUS_SERVER_ADDR=$2
-        if [ -z $PERSEUS_SERVER_ADDR ]; then
+        if [ -z "$PERSEUS_SERVER_ADDR" ]; then
             echo The Perseus server address \(host:port\) is required after -s/--server.
             exit 1
         fi
@@ -48,7 +48,7 @@ fi
 _PERSEUS_MODULE_PATH=$1
 if [ -n "${_PERSEUS_MODULE_PATH}" ] && [ "${_PERSEUS_MODULE_PATH}" != "${PWD}" ]; then
     _PERSEUS_START_DIR=${PWD}
-    cd ${_PERSEUS_MODULE_PATH}
+    cd "${_PERSEUS_MODULE_PATH}"
 else 
     _PERSEUS_MODULE_PATH=${PWD}
 fi
@@ -64,8 +64,8 @@ fi
 
 # let's go
 echo "Processing module at ${_PERSEUS_MODULE_PATH}"
-for t in `git tag --list 'v*' --sort=-v:refname`; do
+for t in $(git tag --list 'v*' --sort=-v:refname); do
     echo "   analyzing ${t} ..."
-    git checkout -q ${t}
-    perseus update --path ${_PERSEUS_MODULE_PATH} --version ${t} ${_INCLUDE_PRERELEASE}
+    git checkout -q "${t}"
+    perseus update --path "${_PERSEUS_MODULE_PATH}" --version "${t}" ${_INCLUDE_PRERELEASE}
 done
