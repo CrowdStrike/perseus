e2275ca83f1be8c98717e73722ca32e71b3244aa
Dylan Bourque <dylan.bourque@crowdstrike.com>
2023-03-16 08:27:57 -0500

    adjusted variable names for clarity

--- internal/server/web/js/data.js
+++ internal/server/web/js/data.js
@@ -10,12 +10,12 @@ const getModuleVersions = (module) => {
   return fetch(
     `${apiBase}/module-versions?module_name=${module}&version_option=all`
     )
-    .then((data) => data.json())
-    .then((resp) => {
+    .then((resp) => resp.json())
+    .then((data) => {
       // API response structure is 1-element array of modules
       //  {"modules":[{"name": "github.com/example/foo", "versions":["v0.1.0", "v0.2.0", ...]}]}
       //
-      return resp.modules[0].versions
+      return data.modules[0].versions
     });
 };
 
@@ -23,8 +23,8 @@ const getModuleDeps = (module, version, direction) => {
   return fetch(
     `${apiBase}/modules-dependencies?module_name=${module}&version=${version}&direction=${direction}`
   )
-    .then((data) => data.json())
-    .then((resp) => {
+    .then((resp) => resp.json())
+    .then((data) => {
       // API response structure is an array of modules that are direct dependencies/dependants of
       // the current module, each with a single version
       //  {"modules":[{"name": "github.com/example/foo", "versions":["v0.1.0"]}, ...]}
@@ -34,7 +34,7 @@ const getModuleDeps = (module, version, direction) => {
       let out = { nodes: [], links: [] };
       out.nodes.push({ id: fqmn, label: fqmn, level: 0 });
 
-      resp.modules.forEach((mod) => {
+      data.modules.forEach((mod) => {
         const name = `${mod.name}@${mod.versions[0]}`;
         out.nodes.push({ id: name, label: name, level: 1 });
         out.links.push({ source: fqmn, target: name });
