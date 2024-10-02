febd86f6fcccc1a20bcdc172f0e23eed71054f83
Dylan Bourque <dylan.bourque@crowdstrike.com>
2024-10-02 11:48:34 -0500

    add created_at column with associated indexes to module and module_version tables

--- internal/store/create_database.sql
+++ internal/store/create_database.sql
@@ -6,16 +6,22 @@ CREATE TABLE module (
     id              SERIAL,
     name            TEXT NOT NULL,
     description     TEXT,
+    created_at      TIMESTAMP NOT NULL DEFAULT (TIMESTAMP('utc', now())),
     CONSTRAINT pk_module
         PRIMARY KEY(id),
     CONSTRAINT uc_module_name
         UNIQUE(name)
 );
 
+CREATE INDEX ix_module_created_at
+    ON module USING btree
+    (created_at DESC);
+
 CREATE TABLE module_version (
     id          SERIAL,
     module_id   INTEGER NOT NULL,
     version     SEMVER NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT (TIMESTAMP('utc', now())),
     CONSTRAINT pk_module_version
         PRIMARY KEY(id),
     CONSTRAINT uc_module_version_module_id_version
@@ -25,6 +31,10 @@ CREATE TABLE module_version (
         ON UPDATE NO ACTION
         ON DELETE CASCADE
 );
+
+CREATE INDEX ix_module_version_created_at
+    ON module_module USING btree
+    (created_at DESC);
 
 CREATE INDEX idx_module_version_version
     ON module_version USING btree
